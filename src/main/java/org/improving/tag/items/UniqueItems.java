49211ac995ca8902f9c0ee4cece7f11b2f498688
Taylor Cates <Taylor.Cates@improving.com>
2019-10-03 09:30:48 -0500

    added inventory sorting.

--- src/main/java/org/improving/tag/items/UniqueItems.java
+++ src/main/java/org/improving/tag/items/UniqueItems.java
@@ -2,10 +2,10 @@ package org.improving.tag.items;
 
 public enum UniqueItems implements Item {
 
-    THE_ONE_RING("The one Ring.","A golden ring oozing with cheesy power") {
+    THE_ONE_RING("The one Ring","A golden ring oozing with cheesy power") {
     },
     BEYBLADE("A common Beyblade", "Super sick beyblade that was all the rage with the 12 year old crowd in the mid 2000s"),
-    WEIRD_EARS("A pair of strange ears.", "Some weird looking ears. Idk, man."),
+    WEIRD_EARS("A pair of strange ears", "Some weird looking ears. Idk, man."),
     FOX_EAR("Fox Ear","A fluffy red ear with a serrated bloody gash where it was \n brutally ripped off. Brings good luck!"),
     BLUE_SHELL("Blue Shell", "A blue shell with bloody white horns jutting out of its carapace. Metal."),
     NOTHING("", "") {
